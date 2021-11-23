860c7a4f79eb02031c3404aca32ef84c8bb2f1d0
Loïc Montagne <loic.montagne@oyosoft.fr>
2021-11-23 15:40:27 +0100

    Ajout correction exercice 2

--- Sockets/Exercice 2.cpp
+++ Sockets/Exercice 2.cpp
@@ -14,13 +14,17 @@
 int main()
 {
 	WSADATA WSAData;
-	SOCKET sock_fd;         // sock_fd : point de connexion
-	SOCKET new_fd;          // new_fd : socket de communication
-	SOCKADDR_IN my_addr;    // adresse de transport de la socket coté serveur
-	SOCKADDR_IN their_addr; // adresse de transport de la socket coté client
-	unsigned int sin_size;  // Taille d'une adresse de transport
-	char date_string[100];  // Chaine de caractere contenant la date
-	SYSTEMTIME time_value;
+	SOCKET sock_fd;                   // sock_fd : point de connexion
+	SOCKET new_fd;                    // new_fd : socket de communication
+	SOCKADDR_IN my_addr;              // adresse de transport de la socket coté serveur
+	SOCKADDR_IN their_addr;           // adresse de transport de la socket coté client
+	unsigned int sin_size;            // Taille d'une adresse de transport
+	char buffer[MAXDATASIZE];         // Message recu
+	int length;                       // Taille du message reçu
+	char date_string[MAXDATASIZE];    // Chaine de caractere contenant la date
+	time_t time_value;
+	struct tm* time_info;
+
 
 	// Appel propre à Windows : sert à initialiser l'utilisation des WinSocks par un processus
 	WSAStartup(MAKEWORD(2, 0), &WSAData);
@@ -45,9 +49,8 @@ int main()
 	// Boucle principale de traitement des demandes client
 	while (1)
 	{
-		int sinsize = sizeof(their_addr);
-
 		// Accept est bloquant : le processus n'avance pas tant que personne ne se connecte
+		int sinsize = sizeof(their_addr);
 		if ((new_fd = accept(sock_fd, (SOCKADDR*)&their_addr, &sinsize)) == -1)
 			perror("Acceptation d'une connexion entrante : ");
 
@@ -55,25 +58,67 @@ int main()
 		printf("server: got connection from %s\n",
 			inet_ntoa(their_addr.sin_addr)); // inet_ntoa : net to ascii
 
-	 // On recupere l'heure et la date
-		GetLocalTime(&time_value);
-		sprintf(date_string,
-			"%d-%02d-%02d %02d:%02d:%02d.%03d",
-			time_value.wYear,
-			time_value.wMonth,
-			time_value.wDay,
-			time_value.wHour,
-			time_value.wMinute,
-			time_value.wSecond,
-			time_value.wMilliseconds);
+		// On boucle jusqu'à ce que le client se déconnecte
+		while (1)
+		{
+			// On attend une demande du client
+			if ((length = recv(new_fd, buffer, MAXDATASIZE, 0)) == -1)
+			{
+				// S'il y eu une erreur, c'est certainement que le client s'est déconnecté
+				// On affiche l'erreur
+				perror("Reception d'un message : ");
+				// On ferme la socket de communication
+				if (closesocket(new_fd) == -1)
+					perror("Fermeture de la socket de communication : ");
+				// Et on quitte la boucle de traitement des messages du client
+				break;
+			}
 
-		// On envoie l'heure et la date
-		if (send(new_fd, date_string, strlen(date_string), 0) == -1)
-			perror("Envoi de la date et de l'heure : ");
+			// Suppression des retours chariots (\n et \r)
+			while (length > 0 && (buffer[length - 1] == '\n' || buffer[length - 1] == '\r'))
+				length--;
+			// Ajout de backslash zero a la fin pour en faire une chaine de caracteres
+			if (length >= 0 && length < MAXDATASIZE)
+				buffer[length] = '\0';
 
-		if (closesocket(new_fd) == -1)
-			perror("Fermeture de la socket de communication : ");
+			// Affichage du message
+			printf("Message recu : %s \n", buffer);
 
+			// On recupere l'heure et la date
+			time(&time_value);
+			time_info = localtime(&time_value);
+
+			// Traitement du message reçu
+			if (strcmp(buffer, "DATE") == 0)
+			{
+				strftime(date_string, MAXDATASIZE, "%e/%m/%Y", time_info);
+				// On envoie l'heure et la date
+				if (send(new_fd, date_string, strlen(date_string), 0) == -1)
+					perror("Envoi d'un message : ");
+			}
+			else if (strcmp(buffer, "JOUR") == 0)
+			{
+				strftime(date_string, MAXDATASIZE, "%A", time_info);
+				// On envoie l'heure et la date
+				if (send(new_fd, date_string, strlen(date_string), 0) == -1)
+					perror("Envoi d'un message : ");
+			}
+			else if (strcmp(buffer, "MOIS") == 0)
+			{
+				strftime(date_string, MAXDATASIZE, "%B", time_info);
+				// On envoie l'heure et la date
+				if (send(new_fd, date_string, strlen(date_string), 0) == -1)
+					perror("Envoi d'un message : ");
+			}
+			else if (strcmp(buffer, "QUITTER") == 0)
+			{
+				// On ferme la socket de communication
+				if (closesocket(new_fd) == -1)
+					perror("Fermeture de la socket de communication : ");
+				// Et on quitte la boucle de traitement des messages du client
+				break;
+			}
+		}
 	}
 
 	/* On devrait faire closesocket(sock_fd); puis WSACleanup(); mais puisqu'on a entré une boucle infinie ... */
